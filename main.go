c24bd4177f2435cfa69914c605379fc9d8d6fb3a
Mohamed Wael <mohamedwael.31.8.2000@gmail.com>
2022-07-04 21:23:34 +0200

    test saveToFile func

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
 	cards := new_deck()
 
 	fmt.Println(cards.toString())
+
+	err := cards.saveToFile("my_cards")
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
