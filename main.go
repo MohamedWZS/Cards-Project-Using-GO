d226f344f14fadc877d28b0533f01b26e172ab35
Mohamed Wael <mohamedwael.31.8.2000@gmail.com>
2022-07-04 22:31:21 +0200

    add newDeckFromFile func

--- main.go
+++ main.go
@@ -1,18 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"log"
-)
-
 func main() {
-	cards := new_deck()
-
-	fmt.Println(cards.toString())
-
-	err := cards.saveToFile("my_cards")
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
